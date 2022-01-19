6b0a2b694216fd0e5c4aaa6170d12edda1d86ab3
Osman Makhtoomnezhad <77289475+osmanmakhtoom@users.noreply.github.com>
2022-01-19 14:30:05 +0330

    This module is for calculating and returning results

---
+++ modules/calculator.py
@@ -0,0 +1,11 @@
+def calculate(number1, number2, operator):
+    if operator == '+':
+        return number1 + number2
+    elif operator == '-':
+        return number1 - number2
+    elif operator == '*':
+        return number1 * number2
+    elif operator == '/':
+        return number1 / number2
+    else:
+        return "من فقط چهار عمل رو بلدم!"
